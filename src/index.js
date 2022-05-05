475f572a330e3c1ae2bcc066f0a51e4cb1eb4775
erichvorster <erichvorster1@gmail.com>
2022-05-05 22:09:32 +0200

    13th ommit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Swiper from "swiper";
+
 const buttonsContainer = document.querySelector(
   ".features__buttons__container"
 );
@@ -140,3 +142,31 @@ const slider = function () {
   });
 };
 slider();
+
+//Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
